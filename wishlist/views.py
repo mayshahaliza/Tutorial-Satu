2c3fb61f545e97da277dbb247b24159e36a2f69e
maysha.haliza <maysha.haliza@ui.ac.id>
2022-09-08 14:30:17 +0700

    Please enter the commit message for your changes. Lines starting

---
+++ wishlist/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from wishlist.models import ItemWishlist
+...
+
+# Create your views here.
+def show_wishlist(request):
+    data_wishlist_item = ItemWishlist.objects.all()
+    context = {
+    'list_item': data_wishlist_item,
+    'name': 'Maysha Haliza Kirana'
+    }
+    return render(request, "wishlist.html", context)
+    
+    
